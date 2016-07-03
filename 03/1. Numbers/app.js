10c433597f696e58cd94451dc6d25606d946eac6
bkzb1zz <tishovrbg@gmail.com>
2016-07-03 18:12:17 +0300

    TEST TEST

--- 03/1. Numbers/app.js
+++
@@ -1,9 +0,0 @@
-function solve(args) {
-    var input = +args[0];
-    var concat = '';
-
-    for (var index = 1; index <= input; index += 1) {
-        concat += index + ' ';
-    }
-    console.log(concat);
-}
